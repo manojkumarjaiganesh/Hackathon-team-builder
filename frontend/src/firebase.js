eb284acce61eb606d9806db814eba38f62aeccba
manojkumarjaiganesh <manojkumarjaiganesh@gmail.com>
2026-04-08 21:10:35 +0530

    Update

---
+++ frontend/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCYGMNaZ8EkHGExu_dJ_i2DcHBCfz3d22k",
+  authDomain: "hackathon-team-builder-2cfa1.firebaseapp.com",
+  projectId: "hackathon-team-builder-2cfa1",
+  storageBucket: "hackathon-team-builder-2cfa1.firebasestorage.app",
+  messagingSenderId: "594660565846",
+  appId: "1:594660565846:web:4407bb6d73b35f9d742cf7",
+  measurementId: "G-M7HYRB58RC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
